46bb6147639a701eb1fcb4051fcba65e817eceb2
Brian" Tatsuya Moriguchi <brian@beckos.com>
2019-12-31 23:57:28 -0800

    Initial Commit

---
+++ URLSessionJSONRequest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  URLSessionJSONRequest
+//
+//  Created by Brian Moriguchi on 12/31/19.
+//  Copyright © 2019 Brian Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
